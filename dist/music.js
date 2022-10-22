64fc017c07c886c690534578c3629eb50f3f0672
fast2021 <qingxinxw@hotmail.com>
2022-10-22 23:30:50 +0800

    Site updated: 2022-10-22 23:30:47

--- dist/music.js
+++ dist/music.js
@@ -4,15 +4,33 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
       {
-        name: "Ancient Legends",
-        artist: 'Tony chen',
-        url: 'https://3x192928s1.eicp.vip/Share/Ancient%20Legends.m4a',
+        name: "喜欢你",
+        artist: 'Beyond',
+        url: 'https://3x192928s1.eicp.vip/directlink/2/%E9%9F%B3%E4%B9%90/Beyond%20-%20%E5%96%9C%E6%AC%A2%E4%BD%A0.mp3',
         cover: 'https://oeff2vktt.bkt.clouddn.com/image/84.jpg',
       },
       {
-        name: 'Hope',
-        artist: 'Tony chen',
-        url: 'https://3x192928s1.eicp.vip/Share/Hope.m4a',
+        name: '曾经拥有',
+        artist: 'Beyond',
+        url: 'https://3x192928s1.eicp.vip/directlink/2/%E9%9F%B3%E4%B9%90/Beyond-%E6%9B%BE%E7%BB%8F%E6%8B%A5%E6%9C%89.mp3',
+        cover: 'https://oeff2vktt.bkt.clouddn.com/image/8.jpg',
+      },
+      {
+        name: '海阔天空',
+        artist: 'Beyond',
+        url: 'https://3x192928s1.eicp.vip/directlink/2/%E9%9F%B3%E4%B9%90/Beyond%20-%20%E6%B5%B7%E9%98%94%E5%A4%A9%E7%A9%BA.mp3',
+        cover: 'https://oeff2vktt.bkt.clouddn.com/image/8.jpg',
+      },
+      {
+        name: '遥望',
+        artist: 'Beyond',
+        url: 'https://3x192928s1.eicp.vip/directlink/2/%E9%9F%B3%E4%B9%90/Beyond%20-%20%E9%81%A5%E6%9C%9B.mp3',
+        cover: 'https://oeff2vktt.bkt.clouddn.com/image/8.jpg',
+      },
+     {
+        name: '光辉岁月',
+        artist: 'Beyond',
+        url: 'https://3x192928s1.eicp.vip/directlink/2/%E9%9F%B3%E4%B9%90/beyond%20-%20%E5%85%89%E8%BE%89%E5%B2%81%E6%9C%88.mp3',
         cover: 'https://oeff2vktt.bkt.clouddn.com/image/8.jpg',
       },
     ]
